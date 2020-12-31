55b31389eac2f0a777d9b8a61f8051cc3ec8c6d9
Taen Ahammed <taenahammed7@gmail.com>
2020-12-31 16:03:12 +0600

    Computed properties.

---
+++ Computed Properties and Watchers/app.js
@@ -0,0 +1,20 @@
+Vue.createApp({
+    data() {
+        return {
+            author: {
+                name: "John Doe",
+                books: [
+                    "Vue 2 - Advanced Guide",
+                    "Vue 3 - Basic Guide",
+                    "Vue 4 - The Mystery",
+                ],
+            },
+        };
+    },
+
+    computed: {
+        publishedBooksMessage() {
+            return this.author.books.length > 0 ? "Yes" : "No";
+        },
+    },
+}).mount("#computed-basics");
