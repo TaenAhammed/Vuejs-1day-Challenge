fddf2770920399d619d4843324b9cdb722705d56
Taen Ahammed <taenahammed7@gmail.com>
2021-01-01 17:01:30 +0600

    Passing Data to Child Components with Props

--- Components Basics/app.js
+++ Components Basics/app.js
@@ -1,4 +1,16 @@
-const app = Vue.createApp({});
+const App = {
+    data() {
+        return {
+            posts: [
+                { id: 1, title: "My journey with Vue" },
+                { id: 2, title: "Blogging with Vue" },
+                { id: 3, title: "Why Vue is so fun" },
+            ],
+        };
+    },
+};
+
+const app = Vue.createApp(App);
 
 app.component("blog-post", {
     props: ["title"],
