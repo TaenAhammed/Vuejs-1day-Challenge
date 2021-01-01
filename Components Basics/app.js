116c1919f23ab4b824d27c56a8c498f2e45a14ef
Taen Ahammed <taenahammed7@gmail.com>
2021-01-01 19:02:30 +0600

    Content Distribution with Slots

--- Components Basics/app.js
+++ Components Basics/app.js
@@ -1,29 +1,12 @@
-const App = {
-    data() {
-        return {
-            posts: [
-                { id: 1, title: "My journey with Vue" },
-                { id: 2, title: "Blogging with Vue" },
-                { id: 3, title: "Why Vue is so fun" },
-            ],
-            postFontSize: 1,
-        };
-    },
-};
+const app = Vue.createApp({});
 
-const app = Vue.createApp(App);
-
-app.component("blog-post", {
-    props: ["title"],
-
+app.component("alert-box", {
     template: `
-        <div class='blog-post'>
-            <h4>{{title}}</h4>
-            <button @click="$emit('enlargeText')">
-                Enlarge text
-            </button>
+        <div class='demo-alert-box'>
+            <strong>Error!</strong>
+            <slot></slot>
         </div>
     `,
 });
 
-app.mount("#blog-posts-events-demo");
+app.mount("#slots-demo");
